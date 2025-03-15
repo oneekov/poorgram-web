565b693862494f96743e1c73330036de04f1d6f7
oneekov <73463642+oneekov@users.noreply.github.com>
2025-03-15 17:39:24 +0000

    Добавление DEBUG флага

--- app/app.py
+++ app/app.py
@@ -32,4 +32,4 @@ async def proxy():
     return ''
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000, debug=DEBUG)
--- app/config.py
+++ app/config.py
@@ -3,5 +3,6 @@ import os
 
 API_ID = int(os.environ.get('API_ID', 0))
 API_HASH = os.environ.get('API_HASH', '')
+DEBUG = bool(os.environ.get("DEBUG", False))
 
 app = Quart(__name__)
